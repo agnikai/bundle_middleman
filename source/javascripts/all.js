42e3694aadd85512a699d25c9958cb388075d8c0
Jacquelyn Carver <jacquelyn905@gmail.com>
2015-02-21 19:30:15 -0500

    Added onClick action for group/event creation buttons.

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,12 @@
 //= require_tree .
+
+$(document).ready(function() {
+	$("#create-group-button").click(function() {
+		$("#default-group-body").hide();
+		$("#create-group").show();
+	});
+	$("#create-event-button").click(function() {
+		$("#default-event-body").hide();
+		$("#create-event").show();
+	});
+});
